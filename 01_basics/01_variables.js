fefe7acb5b93211bb81654dfd753e19efffbc62f
Charleshansda <charles4urs@gmail.com>
2023-12-05 13:48:28 +0000

    editing

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const acntId = 45
+let passId = 258674
+var emlId = "subu"
+ctyId = "Jalandhar"
+let acntSt;
+// acntId = 30
+// passId = 254231
+// emlId = "anil"
+// ctyId = "ranchi"
+
+console.log (acntSt);
