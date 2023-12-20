27769a278a82d02b53310452478299691f9752fc
Charleshansda <charles4urs@gmail.com>
2023-12-20 07:29:57 +0000

    typeof

--- 01_basics/dtyp.js
+++ 01_basics/dtyp.js
@@ -1,3 +1,10 @@
-"use strict";
-console.log (3+3);
-console.log("subodh")
+// "use strict";
+// console.log (3+3);
+// console.log("subodh")
+
+let name ="Subodh";
+let age = 18;
+let isLoggedIn = false;
+
+console.log (typeof undefined)
+console.log (typeof null)
